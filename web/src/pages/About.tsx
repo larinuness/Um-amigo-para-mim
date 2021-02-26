47bcddf22186c0b7f09a029ad81d5f1c1369345a
Larissa <lalanunnes@gmail.com>
2021-02-26 19:17:40 -0300

    Projeto em andamento

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import '../styles/global.css'
-import picAbout from '../images/about.jpg'
+import picAbout from '../images/Anna.jpg'
 import '../styles/pages/aboutpage.css'
 
 
@@ -10,25 +10,28 @@ export default function About() {
     return (
         <div id="aboutpage">
             <Navbar />
-        
-            <h1>Sobre mim</h1>
 
-            <main>
-                
-                <div className="aboutpic">
-                <img src={picAbout} alt="Imagem cachorro pagina sobre" />
-                </div>
-                    <div className="box-model">
-                    
-                    <p>Olá, eu me chamo Larissa. Resolvi fazer esse projeto para praticar um poucos de React e Node,
-                    e para deixar mais divertido resolvi desenvolver com o tema que eu amo muito, os animais. O projeto
-                    não tem nenhuma funcionalidade inovadora, foi feito apenas para praticar.</p>
-                    </div>
-                  
-            </main>
-              
-         <Footer />  
-        </div> 
+            <h1>Sobre mim</h1>
        
+            <div className="content-box">
+                     
+                        <div className="pic">
+                        <img src={picAbout} alt="Foto sobre" />
+                        <div className="informations">
+                        <p>Olá, eu me chamo Larissa. Resolvi fazer esse projeto para praticar um poucos de React e Node,
+                        e para deixar mais divertido resolvi desenvolver com o tema que eu amo muito, os animais. O projeto
+                        não tem nenhuma funcionalidade inovadora, foi feito apenas para praticar.</p>
+                        </div> 
+                        </div>                                              
+                    </div>
+
+            
+
+                
+               
+       
+            <Footer />
+        </div>
+
     )
 }
--- web/src/pages/Contact.tsx
+++ web/src/pages/Contact.tsx
@@ -2,17 +2,31 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import '../styles/global.css'
 import Footer from '../components/Footer'
+import picContact from '../images/picContact.jpg'
+import '../styles/pages/contactpage.css'
 
 export default function Contact() {
     return (
         <div id="contactpage">
 
             <Navbar />
-            <main>
-    
+            <h1>Contato</h1>
 
+         
 
-            </main>
+                    <div className="content-box">
+                     
+                        <div className="pic">
+                        <img src={picContact} alt="Foto contato" />
+                        <div className="informations">
+                        <h3>Para mais informações ou dúvidas sobre o projeto</h3>
+                        <h4>Email:</h4><p>lalanunnes@gmail.com</p>
+                        <h4>Telefone:</h4><p>(11) 94939-9308</p>
+                        <h4>Linkedin:</h4><a href="https://www.linkedin.com/in/larissa-nunes-331900168/">Larissa Nunes</a>
+                        </div> 
+                        </div>                                              
+                    </div>
+              
 
 
 
